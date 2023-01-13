a86568aeaa9abc8236dd70e62b7bc8b0e0680d43
Daniel Norberg <dano@spotify.com>
2023-01-13 21:42:37 +0900

    d12p2

--- src/bin/d12.rs
+++ src/bin/d12.rs
@@ -1,14 +1,27 @@
 use std::collections::VecDeque;
 use std::i32;
 
-use itertools::Itertools;
+use itertools::{enumerate, Itertools};
 
 const INPUT: &str = include_str!("../../input/d12.txt");
 
 fn main() {
     let mut game = parse(INPUT);
-    let dst = bfs(&mut game);
-    println!("{:#?}", dst);
+
+    {
+        let start = game.start;
+        let dst = bfs(&mut game, start).unwrap();
+        println!("{:#?}", dst);
+    }
+
+    {
+        let starts = find_starts(&game.map);
+        let min_dst = starts.iter().flat_map(|start| {
+            reset(&mut game);
+            bfs(&mut game, *start)
+        }).min().unwrap();
+        println!("{:#?}", min_dst);
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -35,10 +48,19 @@ struct Game {
     height: i32,
 }
 
-fn bfs(game: &mut Game) -> i32 {
+fn reset(game: &mut Game) {
+    for row in &mut game.map {
+        for s in row {
+            s.n = 0;
+            s.v = false;
+        }
+    }
+}
+
+fn bfs(game: &mut Game, start: Point) -> Option<i32> {
     let mut q = VecDeque::<Point>::new();
-    game.map[game.start.y as usize][game.start.x as usize].v = true;
-    q.push_back(game.start);
+    game.map[start.y as usize][start.x as usize].v = true;
+    q.push_back(start);
     while !q.is_empty() {
         let s = q.pop_front().unwrap();
         let cz;
@@ -49,7 +71,7 @@ fn bfs(game: &mut Game) -> i32 {
             cn = cs.n;
         }
         if s == game.end {
-            return cn;
+            return Some(cn);
         }
         for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
             let ny = s.y + dy;
@@ -69,7 +91,7 @@ fn bfs(game: &mut Game) -> i32 {
             q.push_back(Point { x: nx, y: ny });
         }
     }
-    panic!("Path not found!");
+    None
 }
 
 fn parse(s: &str) -> Game {
@@ -94,6 +116,16 @@ fn parse(s: &str) -> Game {
     Game { map, start: start.unwrap(), end: end.unwrap(), width, height }
 }
 
+fn find_starts(map: &Map) -> Vec<Point> {
+    enumerate(map)
+        .flat_map(|(y, r)|
+            enumerate(r)
+                .filter(|(_, s)| s.z == 0)
+                .map(|(x, _)| Point { x: x as i32, y: y as i32 })
+                .collect_vec())
+        .collect_vec()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -103,7 +135,19 @@ mod tests {
     #[test]
     fn test1() {
         let mut game = parse(SAMPLE1);
-        let dst = bfs(&mut game);
+        let start = game.start;
+        let dst = bfs(&mut game, start);
         println!("{:#?}", dst);
+    }
+
+    #[test]
+    fn test2() {
+        let mut game = parse(SAMPLE1);
+        let starts = find_starts(&game.map);
+        let min_dst = starts.iter().flat_map(|start| {
+            reset(&mut game);
+            bfs(&mut game, *start)
+        }).min().unwrap();
+        println!("{:#?}", min_dst);
     }
 }
